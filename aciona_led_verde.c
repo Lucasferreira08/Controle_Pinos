b0e6e417c1596fde1ce288d075cc1ead50077ba7
Danilo Moura Pereira <danilo.mourapereira@gmail.com>
2025-01-19 11:06:12 -0300

    Atualiza biblioteca do LED VERDE

--- aciona_led_verde.c
+++ aciona_led_verde.c
@@ -3,36 +3,20 @@
 
 #define GPIO_LED_VERDE 11
 
-void setup() {
+void setup_led_verde() {
     gpio_init(GPIO_LED_VERDE);
     gpio_set_dir(GPIO_LED_VERDE, GPIO_OUT);
 }
 
 int aciona_led_verde() {
     // Chama função de inicialização da GPIO
-    setup();
+    setup_led_verde();
 
     // Inicializa todos os códigos stdio padrão que estão ligados ao binário
     stdio_init_all();
 
-    bool led_state = gpio_get(GPIO_LED_VERDE); // Estado atual do LED
-
-    while (true) {
-        char tecla = '\0';
-
-        // Verifica se alguma tecla do teclado foi pressionada
-        if (scanf("%c", &tecla) == 1) {
-            if (tecla == 'D') { // Verifica se a tecla "D" foi pressionada
-                gpio_put(GPIO_LED_VERDE, !led_state); // Liga o LED
-            }
-        }
-
-        // Se nenhuma tecla está sendo pressionada, desliga o LED
-        if (led_ligado && tecla != 'A') {
-            gpio_put(LED_VERMELHO_GPIO, 0); // Desliga o LED
-            led_ligado = false;
-        }
-        sleep_ms(200);
+    if (scanf("%c", &tecla) == 1) {
+        gpio_put(GPIO_LED_VERDE, tecla == 'D'); // Liga o LED se a tecla 'D' estiver pressionada
     }
     return 0;
 }
